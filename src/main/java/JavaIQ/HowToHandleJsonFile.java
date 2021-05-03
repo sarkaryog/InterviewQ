e73e62f54df857df240c6a4f6fb7d035fb0bf204
Yogesh <sarkar.yog@gmail.com>
2021-05-03 16:59:54 +0530

    Added dependency for json

---
+++ src/main/java/JavaIQ/HowToHandleJsonFile.java
@@ -0,0 +1,5 @@
+package JavaIQ;
+
+public class HowToHandleJsonFile {
+
+}
--- src/main/java/SeleniumIQ/HowToHandleCalendar.java
+++ src/main/java/SeleniumIQ/HowToHandleCalendar.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -19,7 +20,8 @@ public class HowToHandleCalendar {
 
 	static WebDriver driver;
 
-	public static void main(String[] args) {
+	@Test
+	public static void handleCalendar() {
 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver(); // Open the browser

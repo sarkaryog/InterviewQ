6993b63ac000adaa5b77e3ed0c58b08477e9999c
Yogesh <sarkar.yog@gmail.com>
2021-05-13 10:38:22 +0530

    HandleJson

--- src/main/java/JavaIQ/HowToHandleJsonFile.java
+++ src/main/java/JavaIQ/HowToHandleJsonFile.java
@@ -1,5 +1,17 @@
 package JavaIQ;
 
 public class HowToHandleJsonFile {
+	
+	//JSON - JavaScript object notation
+	/*
+	 * Popular
+	 * Latest
+	 * Advanced
+	 * Light weight
+	 * Faster
+	 */
+	
+	
+	
 
 }
